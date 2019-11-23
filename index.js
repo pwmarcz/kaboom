564209e88388a0179c1e16754a2022b8bc6c659a
philippeitis <33013301+philippeitis@users.noreply.github.com>
2019-11-23 01:27:44 -0800

    Add boundary checks for width, height, mines
    Checks that width, height > 0 and numMines > 0.

--- index.js
+++ index.js
@@ -663,11 +663,26 @@ function newGame() {
   const debug = document.getElementById('debug').checked;
   const allowOutside = document.getElementById('allowOutside').checked;
 
+  if (width < 0) {
+    alert('Width must be at least 1!');
+    return;
+  }
+  
+  if (height < 0) {
+    alert('Height must be at least 1!');
+    return;
+  }
+
   if (numMines >= width * height) {
     alert('Too many mines!');
     return;
   }
-
+  
+  if (numMines < 0) {
+    alert('Must have at least 1 mine!');
+    return;
+  }
+  
   const gameElement = document.getElementById('game');
   gameElement.innerHTML = '';
   game = new Game(width, height, numMines);
